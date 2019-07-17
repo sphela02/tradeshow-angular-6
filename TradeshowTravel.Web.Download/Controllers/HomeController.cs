9c4880020bce9dd5669fe39ef0067e0cf5eaaf86
Jason Baldwin <Jason.Baldwin@L3Harris.com>
2019-07-17 11:52:33 -0400

    add download project, initial checkin

---
+++ TradeshowTravel.Web.Download/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TradeshowTravel.Web.Download.Controllers
+{
+    public class HomeController : Controller
+    {
+
+    }
+}
