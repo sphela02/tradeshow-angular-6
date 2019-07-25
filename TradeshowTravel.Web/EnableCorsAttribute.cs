d5b6033b7e56e2998d41d6460d6d14ef02c424ad
Jason Baldwin <Jason.Baldwin@L3Harris.com>
2019-07-25 09:55:48 -0400

    configure cors differently

--- TradeshowTravel.Web/EnableCorsAttribute.cs
+++ TradeshowTravel.Web/EnableCorsAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,14 +22,21 @@ namespace TradeshowTravel.Web
                 AllowAnyMethod = true,
                 SupportsCredentials = true
             };
-            foreach (var origin in ConfigurationManager.AppSettings["AllowedOrigins"].Split(';'))
-            {
-                _policy.Origins.Add(origin);
-            }
         }
 
         public Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
+            var policy = new CorsPolicy();
+            var requestUri = request.RequestUri;
+            var authority = requestUri.Authority.ToLowerInvariant();
+            var origins = ConfigurationManager.AppSettings["AllowedOrigins"].Split(';');
+
+            if (origins.Any(x => authority.EndsWith(x)))
+            {
+                var origin = requestUri.GetComponents(System.UriComponents.SchemeAndServer, System.UriFormat.SafeUnescaped);
+                policy.Origins.Add(origin);
+            }
+
             return Task.FromResult(_policy);
         }
     }
