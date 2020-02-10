6667ef88f452218fcecdf951d5823c42e81f42f5
Huang, Guan (Unknown) (Contractor) <ghuang01@harris.com>
2020-02-10 09:39:16 -0500

    correct env variable

--- TradeshowTravel.Client/src/environments/environment.ts
+++ TradeshowTravel.Client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  apiServiceURL: 'http://localhost:4201/tradeshowtravel/api',
+  apiServiceURL: 'http://localhost/tradeshowtravel/api',
   apiPiServiceURL: 'http://localhost/tradeshowtraveldownloads/api',
   imgLibraryURL: 'http://my-dev.harris.com/tradeshowtravel/photos'
 };
