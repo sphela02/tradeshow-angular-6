25827d9f229662bc35405b0305f288c3460ea719
Huang, Guan (Unknown) (Contractor) <ghuang01@harris.com>
2020-02-10 09:26:40 -0500

    c

---
+++ TradeshowTravel.Client/src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiServiceURL: 'api',
+    apiPiServiceURL: 'https://tradeshowtraveldownloads-dev.l3harris.com/api',
+    imgLibraryURL: 'photos'
+  };
--- TradeshowTravel.Client/src/environments/environment.local.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    apiServiceURL: 'api/',
-    apiPiServiceURL: 'http://localhost/tradeshowtraveldownloads/api',
-    imgLibraryURL: 'http://my-dev.harris.com/tradeshowtravel/photos'
-};
-  
