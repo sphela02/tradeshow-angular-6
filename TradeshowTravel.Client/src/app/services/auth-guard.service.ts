c095a82f16bdba61399aff11fcaf4044e6fd1ea6
Huang, Guan (Unknown) (Contractor) <ghuang01@harris.com>
2020-02-12 12:42:02 -0500

    add r type

--- TradeshowTravel.Client/src/app/services/auth-guard.service.ts
+++ TradeshowTravel.Client/src/app/services/auth-guard.service.ts
@@ -3,13 +3,14 @@ import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from
 
 import { AuthService } from '../services/auth.service'
 import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
   constructor(private authService: AuthService) { }
 
-  canActivate(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     let isloggedIn = this.authService.isLoggedIn();
     isloggedIn.subscribe((loggedin) => {
       if (!loggedin && !state.url.includes("auth-callback")) {
