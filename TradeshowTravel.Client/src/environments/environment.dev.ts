ad3d9e8cf786172396ed3e69c3443f818fd93216
Huang, Guan (Unknown) (Contractor) <ghuang01@harris.com>
2020-03-30 19:00:19 -0400

    push to *.l3harris.com

--- TradeshowTravel.Client/src/environments/environment.dev.ts
+++ TradeshowTravel.Client/src/environments/environment.dev.ts
@@ -2,13 +2,13 @@ export const environment = {
   production: false,
   apiServiceURL: 'api',
   apiPiServiceURL: 'https://tradeshowtraveldownloads-dev.harris.com/api',
-  imgLibraryURL: 'https://tradeshowtravel-dev.harris.com/photos',
+  imgLibraryURL: 'https://tradeshowtravel-dev.l3harris.com/photos',
   authority: 'https://sso.l3harris.com/ofisid/api/discovery',
   clientId: 'https://tradeshowtravel-dev.l3harris.com/',
-  clientSecret:"r66T266jpKjkAXR",
-  redirectUri: 'https://tradeshowtravel-dev.harris.com/auth-callback/',
-  postLogoutRedirectUri: 'https://tradeshowtravel-dev.harris.com/',
-  silentRedirectUri: 'https://tradeshowtravel-dev.harris.com/auth-callback?silent=true',
+  clientSecret: "r66T266jpKjkAXR",
+  redirectUri: 'https://tradeshowtravel-dev.l3harris.com/auth-callback/',
+  postLogoutRedirectUri: 'https://tradeshowtravel-dev.l3harris.com/',
+  silentRedirectUri: 'https://tradeshowtravel-dev.l3harris.com/auth-callback?silent=true',
   refererUrlKey: 'referer_url',
-  useSingleSignOn:false
+  useSingleSignOn: true
 };
